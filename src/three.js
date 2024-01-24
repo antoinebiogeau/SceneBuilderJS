4de9ca21238c906d71ad69e08433984118bcacaa
beubeuh <72401464+antoinebiogeau@users.noreply.github.com>
2024-01-24 22:47:44 +0100

    Update three.js

--- src/three.js
+++ src/three.js
@@ -15,7 +15,7 @@ import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass';
 
 
 
-const scene = new THREE.Scene();
+let scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
